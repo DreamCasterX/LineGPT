f441802a217503e7850df7519c3b6401da5bd172
Mike Lu <115346958+DreamCasterX@users.noreply.github.com>
2023-02-21 17:29:57 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -51,7 +51,7 @@ def handle_message(event):
         working_status = False
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text="感謝您的使用，若需要我的服務，請跟我說 「啟動」 謝謝~"))
+            TextSendMessage(text="感謝您的使用，若需要呼叫我，請跟我說 「啟動」 謝謝~"))
         return
     
     if working_status:
